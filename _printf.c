07704b1f20ba3bc56f9dd04ff016ad34766bad3c
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-15 10:41:49 +0000

    added main.h and _printf.c

---
+++ _printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _printf - A function with the same behavior as printf
+ * @format: The string send to this function to print
+ * @...: Any other arguments
+ * Return: how many characters were printed out
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
