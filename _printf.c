e9dd6469e30422d953aba549deffa13f4227e4b6
mohammed salah <mohammed.salah.hassan@hotmail.com>
2023-09-18 12:00:34 +0000

    0

--- _printf.c
+++ _printf.c
@@ -22,29 +22,27 @@ if (format == NULL)
 
 va_start(list, format);
 
-while (format[co] != '\0')
+for (co = 0; format[co] != '\0';co++)
 {
 if (format[co] == '%')
 {
-count++;
+co++;
 	if (format[co] == 'c') /*In case of %c*/
 	{ T += _putchar(va_arg(list, int)); }
 	else if (format[co] == 's') /*In case of %s*/
 	{ T += printstring(va_arg(list, char *)); }
 	else if (format[co] == '%')  /*In case of %%*/
 	{ T += _putchar('%'); }
-	else if (format[co] == 'i' || format[count] == 'd') /*In case of %i*/
+	else if (format[co] == 'i' || format[co] == 'd') /*In case of %i*/
 	{ T += printint(va_arg(list, int)); }
 	else if (format[co] == '\0') /*In case of %'\0'*/
 	{ return (-1); }
 	else /*In case of % followed by non specifiers*/
 	{ T += _putchar(format[co - 1]);
 	T += _putchar(format[co]); }
-	count++;
 }
 	else /*In case of non specifiers*/
-	{ T += _putchar(format[co]);
-	count++; }
+	{ T += _putchar(format[co]); }
 }
 va_end(list);
 return (T);
